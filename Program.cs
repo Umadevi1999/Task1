02f85e490d96e13963c41562815287352f6b84b1
Uma Devi <96137629+Umadevi1999@users.noreply.github.com>
2021-12-24 17:10:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1;
+class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            User[] users  = new User[2];
+           
+        for(int i=0;i<2;i++)
+        {
+            Console.WriteLine("Enter user {0} details:",i+1);
+            string userdetails=Console.ReadLine();
+            string[] userdetails1=userdetails.Split(',');
+            users[i]=new User(userdetails1[0],userdetails1[1],userdetails1[2]);
+        }
+        for(int i=0;i<2;i++)
+        {
+            Console.WriteLine("User {0}",i+1);
+            Console.WriteLine(users[i]);
+
+        }
+        if (users[0].Equals(users[1]))
+        {
+            Console.WriteLine("User 1 is same as User 2");
+        }
+        else
+        {
+            Console.WriteLine("user1 and user 2 are different");
+        }
+            
+               
+        }
+    }
+
+        
+    
---
+++ User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class User
+    {
+        private string username;
+        private string password;
+        private string mailId;
+
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public string MailId { get => mailId; set => mailId = value; }
+
+        public User()
+        {
+
+        }
+
+        public User(string username, string password, string mailId)
+        {
+            this.username = username;
+            this.password = password;
+            this.mailId = mailId;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("username:{0}\nmailId:{1}\n Password:{2}", Username, MailId, Password);
+         }
+    public override bool Equals(object obj)
+        {
+            User obj_user = (User)obj;
+            return this.Username.Equals(obj_user.Username);
+        }
+    public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        }
+
+    }
+
+
+

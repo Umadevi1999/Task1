c65e08765e26348e8943a57294e8fa07d337c0d3
Uma Devi <96137629+Umadevi1999@users.noreply.github.com>
2021-12-24 17:18:11 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,44 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task1;
-class Program
+namespace Task2
+{
+     class Program
     {
+        public static Boolean ValidatePassword(String password)
+        {
+            int validConditions = 0;
+            foreach (char c in password)
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    validConditions++;
+                    break;
+                }
+            }
+            foreach (char c in password)
+            {
+                if (c >= 'A' && c <= 'Z')
+                {
+                    validConditions++;
+                    break;
+                }
+            }
+            if (validConditions == 0) 
+                return false;
+            foreach (char c in password)
+            {
+                if (c >= '0' && c <= '9')
+                {
+                    validConditions++;
+                    break;
+                }
+            }
+            if (validConditions == 1) return false;
+            if (validConditions == 2)
+            {
+                char[] special = { '@', '#', '$', '%', '^', '&', '+', '=' }; // or whatever    
+                if (password.IndexOfAny(special) == -1) return false;
+            }
+            return true;
+        }
+
         static void Main(string[] args)
         {
-            
-            User[] users  = new User[2];
-           
-        for(int i=0;i<2;i++)
-        {
-            Console.WriteLine("Enter user {0} details:",i+1);
-            string userdetails=Console.ReadLine();
-            string[] userdetails1=userdetails.Split(',');
-            users[i]=new User(userdetails1[0],userdetails1[1],userdetails1[2]);
-        }
-        for(int i=0;i<2;i++)
-        {
-            Console.WriteLine("User {0}",i+1);
-            Console.WriteLine(users[i]);
+            Console.WriteLine("Enter the password to be validated:");
+            string password=Console.ReadLine();
 
-        }
-        if (users[0].Equals(users[1]))
-        {
-            Console.WriteLine("User 1 is same as User 2");
-        }
-        else
-        {
-            Console.WriteLine("user1 and user 2 are different");
-        }
-            
-               
+            if (ValidatePassword(password))
+            {
+                Console.WriteLine("Password is valid");
+            }
+            else
+            {
+                Console.WriteLine("Password is not valid");
+            }
         }
     }
-
-        
-    
+}
